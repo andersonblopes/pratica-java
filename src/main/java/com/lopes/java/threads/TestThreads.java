4e6ba4bc73e0cb4f5d4c3b0c78ea39e648df6204
Anderson Lopes <anderson.Lopes@adtalem.com>
2018-05-05 13:40:20 -0300

    Working with threads.

---
+++ src/main/java/com/lopes/java/threads/TestThreads.java
@@ -0,0 +1,52 @@
+package com.lopes.java.threads;
+
+public class TestThreads {
+
+	public static void main(String[] args) {
+		new Thread(t1).start();
+		new Thread(t2).start();
+		new Thread(t3).start();
+		new Thread(t4).start();
+	}
+
+	private static Runnable t1 = new Runnable() {
+
+		@Override
+		public void run() {
+			for (int i = 0; i < 10; i++) {
+				System.out.println("Thread t1: " + i);
+			}
+		}
+	};
+
+	private static Runnable t2 = new Runnable() {
+
+		@Override
+		public void run() {
+			for (int i = 0; i < 10; i++) {
+				System.out.println("Thread t2: " + i);
+			}
+		}
+	};
+
+	private static Runnable t3 = new Runnable() {
+
+		@Override
+		public void run() {
+			for (int i = 0; i < 10; i++) {
+				System.out.println("Thread t3: " + i);
+			}
+		}
+	};
+
+	private static Runnable t4 = new Runnable() {
+
+		@Override
+		public void run() {
+			for (int i = 0; i < 10; i++) {
+				System.out.println("Thread t4: " + i);
+			}
+		}
+	};
+
+}
