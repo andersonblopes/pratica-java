37695c978eac632f4900740ef01c99fce99d9dc2
Anderson Lopes <anderson.lopes@gmail.com>
2019-12-26 21:32:28 +0000

    Preoject refactor.

---
+++ JavaEE/spring-login-custom/src/test/java/com/lopes/springlogincustom/SpringLoginCustomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lopes.springlogincustom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoginCustomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
