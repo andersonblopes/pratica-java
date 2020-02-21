0fb007ac9bc3c4eed3f2d2525522aecefd9e8a7d
Anderson Lopes <anderson.lopes.2010@gmail.com>
2020-02-21 23:25:36 +0000

    Update entity from database.

--- JPA/ecommerce/src/test/java/com/lopes/ecommerce/test/model/ProductPersistTest.java
+++ JPA/ecommerce/src/test/java/com/lopes/ecommerce/test/model/ProductPersistTest.java
@@ -15,9 +15,7 @@ public class ProductPersistTest extends AbstractApplicationTest {
         Product product = new Product(2, "iPhone 11 Pro", "Welcome to the first iPhone powerful enough to be called Pro.", new BigDecimal(999));
 
         entityManager.getTransaction().begin();
-
         entityManager.persist(product);
-
         entityManager.getTransaction().commit();
 
         //Force searching the database
@@ -34,14 +32,33 @@ public class ProductPersistTest extends AbstractApplicationTest {
         Product product = entityManager.find(Product.class, 3);
 
         entityManager.getTransaction().begin();
-
         entityManager.remove(product);
-
         entityManager.getTransaction().commit();
 
         Product product1Removed = entityManager.find(Product.class, 3);
 
         Assert.assertNull(product1Removed);
+    }
+
+    @Test
+    public void mustUpdateProduct() {
+
+        BigDecimal newPrice = new BigDecimal("400.99");
+
+        Product product = entityManager.find(Product.class, 3);
+        product.setPrice(newPrice);
+
+        entityManager.getTransaction().begin();
+        entityManager.merge(product);
+        entityManager.getTransaction().commit();
+
+        //Force searching the database
+        entityManager.clear();
+
+        Product product1Updated = entityManager.find(Product.class, 3);
+
+        Assert.assertNotNull(product1Updated);
+        Assert.assertEquals(newPrice, product1Updated.getPrice());
     }
 
 }
