e24818fe22a1ee935d684adb82982e30b5a0fb54
Anderson Lopes <anderson.lopes.2010@gmail.com>
2020-02-21 21:35:15 +0000

    Test methods to find entities.

--- JPA/ecommerce/src/test/java/com/lopes/ecommerce/test/config/ProductTest.java
+++ JPA/ecommerce/src/test/java/com/lopes/ecommerce/test/config/ProductTest.java
@@ -11,4 +11,23 @@ public class ProductTest extends AbstractApplicationTest{
         Product product = entityManager.find(Product.class, 1);
         Assert.assertNotNull(product);
     }
+
+    @Test
+    public void mustFindOneProductByReference(){
+        Product product = entityManager.getReference(Product.class, 1);
+        Assert.assertNotNull(product);
+        System.out.println("Just execute query, when one property is used.");
+        Assert.assertEquals("Kindle", product.getName());
+    }
+
+    @Test
+    public void mustDiscardChanges(){
+        Product product = entityManager.find(Product.class, 1);
+        product.setName("Iphone X");
+
+        //Restore entity
+        entityManager.refresh(product);
+
+        Assert.assertEquals("Kindle", product.getName());
+    }
 }
