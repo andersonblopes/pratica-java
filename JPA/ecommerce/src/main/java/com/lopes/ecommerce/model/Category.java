119d691eb35bc478b67fd6f55bdb8327d7231eea
Anderson Lopes <anderson.lopes.2010@gmail.com>
2020-02-24 19:56:45 +0000

    Test generated primary key with TABLE strategy.

--- JPA/ecommerce/src/main/java/com/lopes/ecommerce/model/Category.java
+++ JPA/ecommerce/src/main/java/com/lopes/ecommerce/model/Category.java
@@ -12,8 +12,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.TableGenerator;
 
 @Getter
 @Setter
@@ -27,8 +27,9 @@ public class Category {
 
     @EqualsAndHashCode.Include
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq")
-    @SequenceGenerator(name = "seq", sequenceName = "seq_primary_key_generate", initialValue = 10)
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "table")
+    @TableGenerator(name = "table", table = "seq_category", pkColumnName = "categoryId", pkColumnValue = "category",
+            valueColumnName = "next_val")
     private Integer id;
 
     private String name;
