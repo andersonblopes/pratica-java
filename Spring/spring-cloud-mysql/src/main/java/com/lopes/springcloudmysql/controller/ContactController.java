8fd381aab54456ecb51facd1578b7f8b365c46e0
Anderson Lopes <anderson.lopes.2010@gmail.com>
2020-02-29 15:25:04 +0000

    Includes find Contact By Id resource.

--- Spring/spring-cloud-mysql/src/main/java/com/lopes/springcloudmysql/controller/ContactController.java
+++ Spring/spring-cloud-mysql/src/main/java/com/lopes/springcloudmysql/controller/ContactController.java
@@ -1,8 +1,11 @@
 package com.lopes.springcloudmysql.controller;
 
+import com.lopes.springcloudmysql.model.Contact;
 import com.lopes.springcloudmysql.model.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +21,13 @@ public class ContactController {
     @GetMapping
     public List findAll() {
         return repository.findAll();
+    }
+
+    @GetMapping(path = {"/{id}"})
+    public ResponseEntity<Contact> findById(@PathVariable long id) {
+        return repository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
     }
 
 }
