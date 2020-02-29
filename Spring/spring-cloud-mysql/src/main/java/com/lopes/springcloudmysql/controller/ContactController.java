029f107aec47b9f491dcfbef35b262ebd11269a4
Anderson Lopes <anderson.lopes.2010@gmail.com>
2020-02-29 15:26:09 +0000

    Includes create Contact resource.

--- Spring/spring-cloud-mysql/src/main/java/com/lopes/springcloudmysql/controller/ContactController.java
+++ Spring/spring-cloud-mysql/src/main/java/com/lopes/springcloudmysql/controller/ContactController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +30,12 @@ public class ContactController {
         return repository.findById(id)
                 .map(record -> ResponseEntity.ok().body(record))
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+
+    @PostMapping
+    public Contact create(@RequestBody Contact contact) {
+        return repository.save(contact);
     }
 
 }
