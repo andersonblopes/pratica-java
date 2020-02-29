e80b4080c5dbaad7588b63ce0622251f0d6d962c
Anderson Lopes <anderson.lopes.2010@gmail.com>
2020-02-29 14:30:35 +0000

    Includes CRUD REST API Using Spring Boot 2, Hibernate, JPA, and MySQL

---
+++ Spring/spring-cloud-mysql/src/test/java/com/lopes/springcloudmysql/SpringCloudMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lopes.springcloudmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
